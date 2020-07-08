a42715fa8538eb3bacd4ab473122f91b9624c8e0
Eugene White <eugenewhite1994@gmail.com>
2020-07-08 18:27:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
+<<<<<<< HEAD
     elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
     else
@@ -9,3 +10,17 @@ def speak_to_grandma(phrase)
   end
 
 
+=======
+    elsif phrase == phrase.downcase
+  		return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == phrase.upcase
+      return "NO, NOT SINCE 1938!"
+    else
+  	phrase == phrase
+  	return ""
+    end
+  end
+
+speak_to_grandma("'Hi Nana, how are you?")
+
+>>>>>>> a4ac859c953602fa8bc1a65a5cb4bb4fdf15ca1c
